36f4822dbbb6d8abf7c62a4a8a8f53c73b0ad036
David Pratt <dpratt747@gmail.com>
2025-05-20 21:20:01 +0100

    add atomic reference

--- src/main/kotlin/persistence/InMemoryDatabase.kt
+++ src/main/kotlin/persistence/InMemoryDatabase.kt
@@ -3,6 +3,7 @@ package persistence
 import arrow.core.Option
 import arrow.core.firstOrNone
 import domain.*
+import java.util.concurrent.atomic.AtomicReference
 
 interface DBAlg {
     fun store(longUrl: LongUrl, shortUrl: ShortUrl)
@@ -10,24 +11,27 @@ interface DBAlg {
     fun getLongestUrlByShortUrl(url: ShortUrl): Option<LongUrl>
 }
 
-class InMemoryDatabase private constructor(private val state: MutableMap<LongUrl, ShortUrl>) : DBAlg {
+class InMemoryDatabase private constructor(private val state: AtomicReference<MutableMap<LongUrl, ShortUrl>>) : DBAlg {
     override fun store(longUrl: LongUrl, shortUrl: ShortUrl) {
-        state.put(longUrl, shortUrl)
+        state.getAndUpdate { map ->
+            map.put(longUrl, shortUrl)
+            map
+        }
     }
 
     override fun getAll(): Map<LongUrl, ShortUrl> {
-        return state.toMap()
+        return state.get().toMap()
     }
 
     override fun getLongestUrlByShortUrl(url: ShortUrl): Option<LongUrl> {
-        return state.filter { (_, value) ->
+        return state.get().filter { (_, value) ->
             value == url
         }.keys.firstOrNone()
     }
 
     companion object {
         fun make(state: MutableMap<LongUrl, ShortUrl> = mutableMapOf()): DBAlg {
-            return InMemoryDatabase(state)
+            return InMemoryDatabase(AtomicReference(state))
         }
     }
 }
